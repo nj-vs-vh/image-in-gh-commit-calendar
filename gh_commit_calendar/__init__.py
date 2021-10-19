cf521ce45a96d2b002d56ffce7288c70db014e52
Igor Vaiman <gosha.vaiman@gmail.com>
2021-10-19 20:13:02 +0300

    max commits per day option added

--- gh_commit_calendar/__init__.py
+++ gh_commit_calendar/__init__.py
@@ -6,16 +6,27 @@ from shutil import rmtree
 from gh_commit_calendar import git, image2calendar
 
 
-def create_repo_with_image(remote_repo_url: str, local_repo_name: str, image_file: Path, overwrite: bool = True):
-    commit_count_by_date = image2calendar.image2calendar(image_file, max_commits_per_day=30)
+def create_repo_with_image(
+    remote_repo_url: str,
+    local_repo_name: str,
+    image_file: Path,
+    max_commits_per_day: int,
+    overwrite: bool = True,
+):
+    commit_count_by_date = image2calendar.image2calendar(
+        image_file, max_commits_per_day=max_commits_per_day
+    )
 
-    repo = Path(__file__).parent / f'../../gh-commit-calendar-generated-repos/{local_repo_name}'
+    repo = (
+        Path(__file__).parent
+        / f"../../gh-commit-calendar-generated-repos/{local_repo_name}"
+    )
     if overwrite and repo.exists():
         rmtree(repo)
     repo.mkdir(parents=True)
 
     git.init(repo)
-    
+
     readme_generated = False
     n_dates = len(commit_count_by_date)
     for commit_date, commit_count in tqdm(commit_count_by_date.items(), total=n_dates):
--- run.py
+++ run.py
@@ -2,7 +2,8 @@ from gh_commit_calendar import create_repo_with_image
 
 
 create_repo_with_image(
-    remote_repo_url='git@github.com:nj-vs-vh/gh-commit-calendar-image.git',
+    remote_repo_url='git@github.com:nj-vs-vh/gh-commit-calendar-generated-image.git',
     local_repo_name='test',
     image_file='example/hello-world.png',
+    max_commits_per_day=20,
 )
